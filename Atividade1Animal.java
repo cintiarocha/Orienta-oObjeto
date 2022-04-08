b9d2395f19ba645abfd8b4064eb3dd2d7ef89674
unknown <vitor.santana03@hotmail.com>
2022-04-07 22:37:44 -0300

    first commit

---
+++ Atividade1Animal.java
@@ -0,0 +1,44 @@
+package PolimorfismoHerança;
+
+public class Atividade1Animal {
+	private String nome;
+	private int idade;
+	private String som;
+	
+	public Atividade1Animal (String nome, int idade, String som)
+	{
+		this.nome=nome;
+		this.idade=idade;
+		this.som=som;
+				
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getSom() {
+		return som;
+	}
+
+	public void setSom(String som) {
+		this.som = som;
+	}
+
+	
+}
+
+
+
---
+++ Atividade1Cachorro.java
@@ -0,0 +1,11 @@
+package PolimorfismoHerança;
+
+public class Atividade1Cachorro extends Atividade1Animal {
+
+	privat boolean correr;
+	
+		
+	}
+	
+
+
